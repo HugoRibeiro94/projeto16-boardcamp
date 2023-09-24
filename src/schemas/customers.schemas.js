00b454b9156c06204b852bbdfbcac13474b5ba1a
Hugo <hugoo.ribeiro03@gmail.com>
2023-09-24 10:32:54 -0300

    versao teste schemas clientes

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const customersSchema = Joi.object({
     name: Joi.string().required(),
     phone: Joi.string().alphanum().min(10).max(11),
-    cpf: Joi.string().alphanum().length(11),
+    cpf: Joi.string().alphanum().length(11).pattern(/^[0-9]+$/).required(),
     birthday: Joi.date()
 })
